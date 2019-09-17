5452dd0e0bf2173576d97bbeacdabce72a6b2fec
tung nguyen <tungnm72@wru.vn>
2019-09-17 22:19:01 +0700

    Hoàn thành trang chủ

--- JS/jscript.js
+++ JS/jscript.js
@@ -1,1 +1,20 @@
-$('.carousel').carousel()
+// Tao nut back to top
+//Khi người dùng cuộn chuột thì gọi hàm scrollFunction
+window.onscroll = function() {scrollFunction()};
+        // khai báo hàm scrollFunction
+        function scrollFunction() {
+            // Kiểm tra vị trí hiện tại của con trỏ so với nội dung trang
+            if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+                //nếu lớn hơn 500px thì hiện button
+                document.getElementById("myBtn").style.display = "block";
+            } else {
+                 //nếu nhỏ hơn 20px thì ẩn button
+                 document.getElementById("myBtn").style.display = "none";
+             }
+         }
+        //gán sự kiện click cho button
+        document.getElementById('myBtn').addEventListener("click", function(){
+            //Nếu button được click thì nhảy về đầu trang
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        });
